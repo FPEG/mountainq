e978856844b2ac462a4bcbfb24b0ec10dd4217fd
sp <sp@sp.com>
2020-01-11 23:20:37 +0800

    ok todo commit and loop

--- VSProject/steer_adv.h
+++ VSProject/steer_adv.h
@@ -5,11 +5,11 @@
 #define S3010_CH    FTM_CH0   //通道号PTA8
 #define S3010_HZ    50       //舵机的频率
 
-#define SteerMax 850     //舵机最大值
-#define SteerMin  700    //舵机最小值  
-#define SteerMidle 768   //舵机中值
+#define SteerMax 810     //舵机最大值
+#define SteerMin  630    //舵机最小值  
+#define SteerMidle 720   //舵机中值
 
-#define SteerSection 68
+#define SteerSection 90
 
 extern float  LastError;
 extern float  Error;
