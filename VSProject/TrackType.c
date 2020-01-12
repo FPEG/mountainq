c36026df4eb2a9b60e1dce24a586556a99d4bf5d
grey <grey@LAPTOP-F1EDEQ2L>
2020-01-12 15:28:50 +0800

    commit TrackType.c

--- VSProject/TrackType.c
+++ VSProject/TrackType.c
@@ -5,11 +5,13 @@
 
 /*********define for CrossConduct**********/
 unsigned char  CrossFlag = 0;//十字标志
-unsigned char  LastCrossFlag = 0;
-unsigned char  LastLastCrossFlag = 0;
-unsigned char  CrossNumberFalg = 0;
+unsigned char  LastCrossFlag = 0;//记录上上次是否是十字
+unsigned char  LastLastCrossFlag = 0;//记录上一次是否是十字
+unsigned char  CrossNumberFalg = 0;//记录，代表已经进入了十字
 unsigned char  CloseLoopFlag = 0;
-
+/*
+ * 不用的
+ */
 /*********define for RecognitionObstacle**********/
 unsigned char  ObstacleLfetRowStart = 0;
 unsigned char  ObstacleLfetColStart = 0;
@@ -19,7 +21,9 @@ unsigned char  ObstacleRightColStart = 0;
 unsigned char  ObstacleRightOkFlag = 0;
 
 
-//对十字进行识别和进行特殊处理      
+/**
+ * \brief 
+ */
 void NormalCrossConduct(void)
 {
 	unsigned char i;
@@ -29,7 +33,7 @@ void NormalCrossConduct(void)
 	CrossFlag = 0;//清零
 
 
-	if ((AllLose >= 15))//左右同时丢线 
+	if ((AllLose >= 15))//左右同时丢线//左右同时丢线行数大于15//15这个参数感觉要改//15是用来判断十字还是圆环的标志，圆环0，十字1
 	{
 		CrossFlag = 1;//标记十字    
 	}
@@ -66,38 +70,40 @@ void NormalCrossConduct(void)
 
 }
 
-//判断是不是进去了十字回环，如果，则要关闭环道识别，否则斜出十字是，会误判成环道
 
+
+/**
+ * \brief //判断是不是进去了十字回环，如果，则要关闭环道识别，否则斜出十字是，会误判成环道
+ */
 void IsOrNotCrossLoop(void)
 {
-
 	static char CrossLoopErrorNum = 0;
 	static unsigned char CloseLoopNum = 0;
-	static unsigned char CrossErrorClose = 0;//记录多久没有遇到偏差大于10
+	static unsigned char CrossErrorClose = 0;//记录多久没有遇到偏差大于10//？我认为是用单双记录出环入环
 	unsigned char  ErrorCloseFlag = 0;
 
 	ErrorCloseFlag = 0;
 
-	if (LastLastCrossFlag && LastCrossFlag && CrossFlag == 0)//前两次是十字，本次不是十字
+	if (LastLastCrossFlag && LastCrossFlag && CrossFlag == 0)//前两次是十字，本次不是十字//进入十字
 	{
-
 		CrossNumberFalg++;//记录，代表已经进入了十字
-
-
-
 	}
-
+	/*
+	 * 进入了圆环
+	 */
 	if (CrossNumberFalg == 1)
 	{
-		CrossErrorClose++;
+		CrossErrorClose++;//为出圆环准备
 
-		if (CrossErrorClose > 8)
+		if (CrossErrorClose > 8)//？？进了多次环
 		{
 			ErrorCloseFlag = 1;
 			CrossErrorClose = 0;
 
 		}
-
+		/*
+		 * 看不懂
+		 */
 		if (ABS(Error) >= 10 && !ErrorCloseFlag && LastLine >= 20)//避免十字接直道，关闭了环道
 		{
 			CrossLoopErrorNum++;//连续记录偏大于10
@@ -109,7 +115,9 @@ void IsOrNotCrossLoop(void)
 
 		}
 
-
+		/*
+		 * 看不懂
+		 */
 		if (CrossLoopErrorNum >= 4 && CloseLoopFlag == 0)//连续4次偏差大于10
 		{
 			CloseLoopFlag = 1;//关掉圆环处理
@@ -118,7 +126,10 @@ void IsOrNotCrossLoop(void)
 	}
 
 
-
+	/*
+	 * 圆环内多次绕圈处理
+	 * 我们应该不需要
+	 */
 	if (CloseLoopFlag)//开始计算关闭了环道处理的时间
 	{
 
